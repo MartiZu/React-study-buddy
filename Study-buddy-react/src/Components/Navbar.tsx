436206848a233f1ba2ce8655658116d968b65114
MartiZu <martina.zurli@gmail.com>
2024-02-10 22:36:08 +0000

    installation created styles folders, installed sass and starting to create fodler structure

--- Study-buddy-react/src/Components/Navbar.tsx
+++ Study-buddy-react/src/Components/Navbar.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="nav-bar">
+      <Link to="/">
+        <img id="logo" src="/images/studybuddy-logo.png" alt="logo" />
+      </Link>
+    </nav>
+  );
 }
