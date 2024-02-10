74984e24487a2df96dc438f125e6ab9dd31f24cd
MartiZu <martina.zurli@gmail.com>
2024-02-10 22:16:55 +0000

    started structure folder and routing

--- Study-buddy-react/src/app.tsx
+++ Study-buddy-react/src/app.tsx
@@ -1,10 +1,20 @@
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import UserPage from "./pages/UserPage.tsx";
+import Footer from "./Components/Footer.tsx";
 
 export function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <div className="container main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/UserPage" element={<UserPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
 }
