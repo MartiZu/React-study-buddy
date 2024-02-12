17844c57ee418324020005b076c6c2a4082eb6ab
MartiZu <martina.zurli@gmail.com>
2024-02-12 14:34:22 +0000

    removed projects

--- Study-buddy-react/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './styles/index.scss'
-
-render(<App />, document.getElementById('app')!)
--- Study-buddy-react/src/tests/example.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("Example test", () => {
-  it("should pass test", () => {
-    const sum = 2 + 2;
-    expect(sum).toEqual(4);
-  });
-});
